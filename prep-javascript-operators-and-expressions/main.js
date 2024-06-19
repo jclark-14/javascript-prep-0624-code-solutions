169023da5f377763e1b2b166844560bebe982e6d
Jody Clark <jodyclark09@gmail.com>
2024-06-19 13:35:49 +0000

    Completed prep-javascript-operators-and-expressions exercise.

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -33,6 +33,7 @@ const isSparta = numberOfSoldiers === 300;
 console.log('value of isSparta:', isSparta);
 console.log('type of isSparta:', typeof isSparta);
 
-let nickname = fullName + ' is the GOAT';
+let nickname = fullName;
+nickname += ' is the GOAT';
 console.log('value of nickname:', nickname);
 console.log('type of nickname:', typeof nickname);
