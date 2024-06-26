e6e5d07997088522e6abd1edd89351381aa07d0e
Jody Clark <jodyclark09@gmail.com>
2024-06-26 18:16:06 +0000

    Completed prep-array-challenge exercise.

---
+++ prep-array-challenge/src/tail.js
@@ -0,0 +1,8 @@
+/* exported tail */
+function tail(array) {
+  const tailArray = [];
+  for (let index = 1; index < array.length; index++) {
+    tailArray.push(array[index]);
+  }
+  return tailArray;
+}
